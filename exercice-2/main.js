9bc2b95f4c3ac8b550cfa2062afa1917ab0b78c9
rayan-broq <94693010+rayan-broq@users.noreply.github.com>
2022-08-30 14:51:11 +0200

    fin exercice 2

--- exercice-2/main.js
+++ exercice-2/main.js
@@ -2,7 +2,7 @@
 	let lancementOk = false;
 	let niveauDeCarburant = 17000;
 
-	if (niveauDeCarburant >= 20000 {
+	if (niveauDeCarburant >= 20000) {
 		console.log('Niveau de carburant vérifié.');
 		lancementOk = true;
 	} else {
